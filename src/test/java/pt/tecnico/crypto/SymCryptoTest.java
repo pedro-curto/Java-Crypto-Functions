05a8b7aa6fcf8ae5a9cc11fba4cd5f0ad49d6680
Pedro Curto <pedroalexandrecurto@gmail.com>
2024-11-18 22:34:01 +0000

    feat (ex.1): add code for CBC

--- src/test/java/pt/tecnico/crypto/SymCryptoTest.java
+++ src/test/java/pt/tecnico/crypto/SymCryptoTest.java
@@ -4,15 +4,17 @@ import static javax.xml.bind.DatatypeConverter.printHexBinary;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.security.Key;
+import java.security.SecureRandom;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
+import javax.crypto.spec.IvParameterSpec;
 
 import org.junit.jupiter.api.Test;
 
 public class SymCryptoTest {
 	/** Plain text to cipher. */
-	private final String plainText = "This is the plain text!";
+	private final String plainText = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 	/** Plain text bytes. */
 	private final byte[] plainBytes = plainText.getBytes();
 
@@ -23,7 +25,8 @@ public class SymCryptoTest {
 	/**
 	 * Symmetric cipher: combination of algorithm, block processing, and padding.
 	 */
-	private static final String SYM_CIPHER = "AES/ECB/PKCS5Padding";
+	private static final String SYM_CIPHER_ECB = "AES/ECB/PKCS5Padding";
+	private static final String SYM_CIPHER_CBC = "AES/CBC/PKCS5Padding";
 
 	/**
 	 * Secret key cryptography test.
@@ -31,9 +34,9 @@ public class SymCryptoTest {
 	 * @throws Exception because test is not concerned with exception handling
 	 */
 	@Test
-	public void testSymCrypto() throws Exception {
+	public void testSymCryptoECB() throws Exception {
 		System.out.print("TEST '");
-		System.out.print(SYM_CIPHER);
+		System.out.print(SYM_CIPHER_ECB);
 		System.out.println("'");
 
 		System.out.println("Text:");
@@ -50,7 +53,7 @@ public class SymCryptoTest {
 		System.out.println(printHexBinary(key.getEncoded()));
 
 		// get a AES cipher object and print the provider
-		Cipher cipher = Cipher.getInstance(SYM_CIPHER);
+		Cipher cipher = Cipher.getInstance(SYM_CIPHER_ECB);
 		System.out.println(cipher.getProvider().getInfo());
 
 		// encrypt using the key and the plain text
@@ -75,5 +78,63 @@ public class SymCryptoTest {
 
 		System.out.println();
 		System.out.println();
+	}
+
+	@Test
+	public void testSymCryptoCBC() throws Exception {
+		System.out.print("TEST '");
+		System.out.print(SYM_CIPHER_CBC);
+		System.out.println("'");
+
+		System.out.println("Text:");
+		System.out.println(plainText);
+		System.out.println("Bytes:");
+		System.out.println(printHexBinary(plainBytes));
+
+		// get a AES private key
+		System.out.println("Generating AES key...");
+		KeyGenerator keyGen = KeyGenerator.getInstance(SYM_ALGO);
+		keyGen.init(SYM_KEY_SIZE);
+		Key key = keyGen.generateKey();
+		System.out.print("Key: ");
+		System.out.println(printHexBinary(key.getEncoded()));
+
+		// generate initialization vector (IV)
+		IvParameterSpec ivSpec = generateIv();
+		System.out.print("IV: ");
+		System.out.println(printHexBinary(ivSpec.getIV()));
+
+		// get a AES cipher object and print the provider
+		Cipher cipher = Cipher.getInstance(SYM_CIPHER_CBC);
+		System.out.println(cipher.getProvider().getInfo());
+
+		// encrypt using the key and the plain text
+		System.out.println("Ciphering...");
+		cipher.init(Cipher.ENCRYPT_MODE, key, ivSpec);
+		byte[] cipherBytes = cipher.doFinal(plainBytes);
+		System.out.print("Result: ");
+		System.out.println(printHexBinary(cipherBytes));
+
+		// decipher the cipher text using the same key
+		System.out.println("Deciphering...");
+		cipher.init(Cipher.DECRYPT_MODE, key, ivSpec);
+		byte[] newPlainBytes = cipher.doFinal(cipherBytes);
+		System.out.print("Result: ");
+		System.out.println(printHexBinary(newPlainBytes));
+
+		System.out.println("Text:");
+		String newPlainText = new String(newPlainBytes);
+		System.out.println(newPlainText);
+
+		assertEquals(plainText, newPlainText);
+
+		System.out.println();
+		System.out.println();
+	}
+
+	public static IvParameterSpec generateIv() {
+		byte[] iv = new byte[16];
+		new SecureRandom().nextBytes(iv);
+		return new IvParameterSpec(iv);
 	}
 }
